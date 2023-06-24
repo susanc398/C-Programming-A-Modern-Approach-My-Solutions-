48069a432c96efddeea8917bba65ee470db454bc
susanc398 <73446896+susanc398@users.noreply.github.com>
2023-06-24 04:27:54 -0400

    Add files via upload

---
+++ ch2/sol2_2.c
@@ -0,0 +1,20 @@
+/* Computes volume of sphere within 10-meter radius */
+
+#include <stdio.h>
+
+#define SCALE_FACTOR (4.0f/3.0f)
+#define PI 3.14159
+
+int main(void)
+{
+    float volume, radius;
+
+    printf("Enter radius (within 10-meters): ");
+    scanf("%f", &radius);
+
+    volume = SCALE_FACTOR * PI * radius * radius;
+
+    printf("Volume: %.5f m^3\n", volume);
+
+    return 0;
+}
